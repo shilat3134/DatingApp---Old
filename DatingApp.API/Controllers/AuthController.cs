418a227b45d7683003ac9f399197f05266783e59
Ben David <shilatx.ben.david@intel.com>
2020-07-12 01:02:38 +0300

    End of section 3

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,82 @@
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Models;
+using DatingApp.API.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using System.Security.Claims;
+using System;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config)
+        {
+            _repo = repo;
+            _config = config;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            // validate request
+
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+            if (await _repo.UserExist(userForRegisterDto.Username))
+                return BadRequest("username already exist");
+
+            var userToCrete = new User
+            {
+                Username = userForRegisterDto.Username
+            };
+
+            var createdUser = await _repo.Register(userToCrete,userForRegisterDto.Password);
+            return StatusCode(201);
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            //check if exist matchig user in database. toLower cause we store the username in db in lowercase
+            var userFromRepo = await _repo.Login(userForLoginDto.UserName.ToLower(), userForLoginDto.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+             
+             var key = new SymmetricSecurityKey(Encoding.UTF8
+                 .GetBytes(_config.GetSection("AppSettings:Token").Value));
+            
+             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+             var tokenDescriptor = new SecurityTokenDescriptor
+             {
+                 Subject = new ClaimsIdentity(claims),
+                 Expires = DateTime.Now.AddDays(1),
+                 SigningCredentials = creds
+             };
+
+             var tokenHandler = new JwtSecurityTokenHandler();
+
+             var token = tokenHandler.CreateToken(tokenDescriptor); // will return to client
+
+             return Ok(new {
+                 token = tokenHandler.WriteToken(token)
+             });
+        }
+
+    }
+}
